d74b09db5315ad10b51a15a68abad3f4c6f12bab
Hector Gutierrez <gutierrez.h3ctor@gmail.com>
2025-06-09 00:11:50 -0600

    lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Héctor Gutiérrez",                         # Tu nombre
+    author_email="gutierrez.h3ctor@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/h3ctor89/tercerRepo",     # URL del proyecto
+)
